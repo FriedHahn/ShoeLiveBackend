45b22e6b09d6585c6be66dfa6eb9671ebb140071
Friedrich <Hans.Hahn@Student.HTW-Berlin.de>
2025-12-15 17:20:33 +0100

    Bugfix

--- src/main/java/htw/webtech/myapp/persistence/repository/TodoEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package htw.webtech.myapp.persistence.repository;
-
-import htw.webtech.myapp.persistence.entity.TodoEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoEntryRepository extends JpaRepository<TodoEntry, Long> {
-}
